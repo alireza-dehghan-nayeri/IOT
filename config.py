a5d91da86605244194ea7a5acf61cd5ceaf10be5
Alireza Dehghan Nayeri <alireza79.nayeri@gmail.com>
2024-12-19 18:05:54 +0200

    Update and rename first.py to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+ssid = 'Alireza' # wifi (hotspotted) name
+pwd = 'tkkm2554' # wifi (hotspotted) password
+MQTT_BROKER = 'c339f5ffde214f699d87407cac7db049.s1.eu.hivemq.cloud' # broker/server URL
+MQTT_PORT= 8883
+MQTT_USER = 'OuluIot2024'  # access username
+MQTT_PWD = 'OuluIot2024' # access pwd
+RATE = 100 # data collection and publish rate in mili seconds
+
